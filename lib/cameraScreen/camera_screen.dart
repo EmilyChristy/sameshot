17d97444e40ecdab414ddfcf7bfa4e6b421c56e8
EmilyChristy <emily@brainstormcreative.co.uk>
2021-04-27 09:43:00 +0100

    adding main files

---
+++ lib/cameraScreen/camera_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
+}
+
+class _CameraScreenState extends State<CameraScreen> {
+  //final Widget title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text("Monkeys eat bananas");
+  }
+}
