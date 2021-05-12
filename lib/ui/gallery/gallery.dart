4e68962d38e5b4fc20265e87cbaac6638c4a5ae0
EmilyChristy <emily@brainstormcreative.co.uk>
2021-05-12 13:37:11 +0100

    buiding up gallery page

--- lib/ui/gallery/gallery.dart
+++ lib/ui/gallery/gallery.dart
@@ -1,5 +1,6 @@
-import 'package:camera/camera.dart';
+//import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
 
@@ -9,6 +10,9 @@ class Gallery extends StatefulWidget {
 }
 
 class _GalleryState extends State<Gallery> {
+  static const platform = const MethodChannel('samples.flutter.dev/battery');
+  List images;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,12 +20,57 @@ class _GalleryState extends State<Gallery> {
         title: Text('Gallery'),
         centerTitle: true,
       ),
-      body: Container(
-          child: SafeArea(
-              child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        children: [Text("show photos here")],
-      ))),
+      // body: Container(
+      //     child: SafeArea(
+      //         child: Column(
+      //   crossAxisAlignment: CrossAxisAlignment.stretch,
+      //   children: [Text("show photos here")],
+      // ))),
+      body: images != null
+          ? new GridView.count(
+              padding: const EdgeInsets.all(8.0),
+              crossAxisCount: 4,
+              children: List.generate(100, (index) {
+                return Center(
+                  child: Text(
+                    'Item $index',
+                    style: Theme.of(context).textTheme.headline5,
+                  ));
+              
+              },
+              // staggeredTileBuilder: (i) =>
+              //     new StaggeredTile.count(2, i.isEven ? 2 : 3),
+              // mainAxisSpacing: 8.0,
+              // crossAxisSpacing: 8.0,
+            )
+          : new Center(
+              child: new CircularProgressIndicator(),
+            ),
     );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getImages();
+  }
+
+  Future<void> _getImages() async {
+    print('inside get images>>>>>>>>>>>>');
+    List imgs;
+    try {
+      final List result = await platform.invokeMethod('getImages');
+      imgs = result;
+      for (String i in result) {
+        print('>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>$i');
+      }
+    } on PlatformException catch (e) {
+      print("Error");
+    }
+
+    setState(() {
+      images = imgs;
+    });
   }
 }
